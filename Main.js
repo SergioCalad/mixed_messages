155bdd4ff318afc9b50ca5ef23ab2e3c45daf4c1
SergioCalad <sacr.n.m@gmail.com>
2022-12-02 16:02:43 -0500

    'Agregue funcion de frase aleatoria'

--- Main.js
+++ Main.js
@@ -2,12 +2,8 @@ let adj=['good','horrible','bad','amazing','beautiful','boring', 'wonderful', 'p
 let anSubs=['cat','dog','horse','turtle','cheetah','lion','giraffe','gorilla','squirrell'];
 let verbs=['eat','buy','kill','rent','hunt','pursue','sell'];
 
-const ramAdj=()=> adj[Math.floor(Math.random()*adj.length)];
+const ramWord=(array)=> array[Math.floor(Math.random()*array.length)];
 
-const ramAnSubs=()=> anSubs[Math.floor(Math.random()*anSubs.length)];
-
-const ramVerbs=()=> verbs[Math.floor(Math.random()*verbs.length)];
-
-console.log(ramAdj());
-console.log(ramAnSubs());
-console.log(ramVerbs());
+console.log(ramWord(adj));
+console.log(ramWord(anSubs));
+console.log(ramWord(verbs));
